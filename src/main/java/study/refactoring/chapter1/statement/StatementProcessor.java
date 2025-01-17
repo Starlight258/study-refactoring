e280172cc29550c1b23c77968bf082b82db734d3
Mint <audwl03071@gmail.com>
2025-01-17 22:26:33 +0900

    refactor: 포인트 계산 로직 메서드 추출

--- src/main/java/study/refactoring/chapter1/statement/StatementProcessor.java
+++ src/main/java/study/refactoring/chapter1/statement/StatementProcessor.java
@@ -4,7 +4,6 @@ import study.refactoring.chapter1.play.Play;
 import study.refactoring.chapter1.play.PlayType;
 import study.refactoring.chapter1.play.Plays;
 
-// 명세서
 public class StatementProcessor {
 
     private final Invoice invoice;
@@ -22,12 +21,8 @@ public class StatementProcessor {
         double totalAmount = 0;
         int volumeCredits = 0;
         for (Performance performance : invoice.performances()) {
-            // 포인트 적립
-            volumeCredits += Math.max(performance.audience() - 30, 0);
-            // 희극 관객 5명마다 추가 포인트 제공
-            if (playFor(performance).type() == PlayType.comedy) {
-                volumeCredits += performance.audience() / 5;
-            }
+            volumeCredits += calculateVolumeCredits(performance);
+
             // 청구 내역 출력
             result.append(String.format(" %s: $%,.2f", playFor(performance).name(), calculateAmount(performance) / 100))
                     .append(" (")
@@ -38,6 +33,15 @@ public class StatementProcessor {
         result.append(String.format("총액: $%,.2f", totalAmount / 100)).append(System.lineSeparator());
         result.append("적립 포인트: ").append(volumeCredits).append("점").append(System.lineSeparator());
         return result.toString();
+    }
+
+    private int calculateVolumeCredits(final Performance performance) {
+        int result = Math.max(performance.audience() - 30, 0);
+        // 희극 관객 5명마다 추가 포인트 제공
+        if (playFor(performance).type() == PlayType.comedy) {
+            result += performance.audience() / 5;
+        }
+        return result;
     }
 
     private Play playFor(final Performance performance) {
